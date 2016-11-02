a1fe29fc74024f72a1080819f8730b50b350a9b1
mark-williams <markwill@hotmail.co.uk>
2016-11-02 10:56:28 +0000

    Included materialize-css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import 'materialize-css/dist/css/materialize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+
 
 ReactDOM.render(
   <App />,
