ea438e62ae93e103ff3e2ddd9f9336d36f6fc5eb
mark-williams <markwill@hotmail.co.uk>
2016-11-16 09:22:24 +0000

    App component changed to be functional stateless

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import ConfiguratorContainer from './components/ConfiguratorContainer';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <ConfiguratorContainer />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to React</h2>
+    </div>
+    <ConfiguratorContainer />
+  </div>
+);
 
 export default App;
