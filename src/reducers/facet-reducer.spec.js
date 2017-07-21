e366f19a94cdf9cbe8ed61651fe13d72a9b20a23
mark-williams <markwill@hotmail.co.uk>
2017-07-21 11:53:20 +0100

    Extracted test facets into one place

--- src/reducers/facet-reducer.spec.js
+++ src/reducers/facet-reducer.spec.js
@@ -2,15 +2,12 @@ import _ from 'lodash';
 import { optionSelected } from '../actions/index';
 import facetReducer from './facet-reducer';
 import { getOptionDescription, getConfiguredPrice, getChosenColour } from '../selectors/facet-selectors';
+import facetsForTest from '../Utils/testUtils';
 
 describe('facet-reducer', () => {
   let testFacets;
   beforeEach(() => {
-    testFacets = [
-      { id: 1, name: 'Facet 1', options: [{ val: 1, desc: 'Facet1Desc1' }, { val: 2, desc: 'Facet1Desc2' }, { val: 3, desc: 'Facet1Desc3', extraCost: 100 }], selectedOption: 0 },
-      { id: 2, name: 'Facet 2', options: [{ val: 1, desc: 'Facet2Desc1' }, { val: 2, desc: 'Facet2Desc2', extraCost: 200 }, { val: 3, desc: 'Facet2Desc3' }], selectedOption: 0 },
-      { id: 3, name: 'Facet 3', options: [{ val: 1, desc: 'Facet3Desc1', extraCost: 300 }, { val: 2, desc: 'Facet3Desc2' }, { val: 3, desc: 'Facet3Desc3' }], selectedOption: 0 },
-    ];
+    testFacets = facetsForTest;
   });
 
   describe('default behaviour', () => {
